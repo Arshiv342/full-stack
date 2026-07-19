9905c4c5117100fe70b9b16dec25e25034592407
Arshiv342 <arshiv342@gmail.com>
2026-07-20 00:52:19 +0530

    Data Entry

--- ValueAcces.js
+++ ValueAcces.js
@@ -1,3 +1,8 @@
-let odd = [1, 3, 5, 7, 9];
-let even = [2, 4, 6, 8, 10];
-let nums = [...odd, ...even];
+let data = {
+    email: "mast@gmail.com",
+    password: "123abc", 
+
+};
+let dataCopy = {
+    ...data, id: 123
+};
